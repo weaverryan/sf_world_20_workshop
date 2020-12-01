3661c412c1a1547a5c1f18b64f5e967875c31275
Ryan Weaver <ryan@thatsquality.com>
2020-11-30 19:33:04 -0500

    Step 26: Listener: getSubscribedEvents

--- src/Security/LastLoginSubscriber.php
+++ src/Security/LastLoginSubscriber.php
@@ -3,10 +3,18 @@
 namespace App\Security;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LastLoginSubscriber implements EventSubscriberInterface
 {
+    public function onLoginSuccess(LoginSuccessEvent $event)
+    {
+    }
+
     public static function getSubscribedEvents()
     {
+        return [
+            LoginSuccessEvent::class => 'onLoginSuccess',
+        ];
     }
 }
