00739e17957d8d72fad219e437ed678998303942
Ryan Weaver <ryan@thatsquality.com>
2020-11-30 19:33:04 -0500

    Step 27: Listener: getUser()

--- src/Security/LastLoginSubscriber.php
+++ src/Security/LastLoginSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
@@ -9,6 +10,11 @@ class LastLoginSubscriber implements EventSubscriberInterface
 {
     public function onLoginSuccess(LoginSuccessEvent $event)
     {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            throw new \Exception('what the heck?');
+        }
     }
 
     public static function getSubscribedEvents()
